6c5737fece128806fac11a6be0b928dbe278bbb3
mulin88 <murong.lin@gmail.com>
2020-03-26 17:30:50 -0700

    change theme

--- source/conf.py
+++ source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import astropy_sphinx_theme
 sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
 
@@ -32,7 +33,10 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'rinoh.frontend.sphinx'
+    'rinoh.frontend.sphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    # 'sphinxjp.themes.basicstrap'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,6 +46,9 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,7 +56,16 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'bootstrap-astropy'
+
+# html_theme = 'basicstrap'
+
+html_theme_options = {
+    # 'sidebar_span': 4, # 1(min) - 12(max)
+    # 'nosidebar': True,
+    # 'rightsidebar': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
